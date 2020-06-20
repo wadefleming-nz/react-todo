ab7b5c49e1ff7307272dbb74f4f6863ec7702c6c
Wade Fleming <wadefleming2000@hotmail.com>
2020-06-20 18:34:53 +0100

    add basic TodoList component

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <ul>
+      <li>item1</li>
+      <li>item2</li>
+      <li>item3</li>
+    </ul>
+  );
+}
+
+export default TodoList;
